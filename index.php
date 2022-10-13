81de498df57ab5b3e692939ab14233ef33789b3e
lucas93220 <lopezlucas52@gmail.com>
2022-10-13 16:12:39 +0200

    maj

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php
+    
+    include_once './pages/head.inc.php';
+    
+?>
+<body>
+<?php
+    
+    include_once './pages/header.inc.php';
+    
+?>
+    <?php
+    
+    include_once './pages/main.inc.php';
+    
+?>
+<?php
+    
+    include_once './pages/footer.inc.php';
+    
+?>
+</body>
+</html>
---
+++ src/var.inc.php
@@ -0,0 +1,6 @@
+<?php
+     const title = "Vente Pc";
+     $prix = [""];
+  
+   
+?>
