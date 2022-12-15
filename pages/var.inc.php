895ef11ffd5fca477fe07e48c3d53edd782a8edc
lucas93220 <lopezlucas52@gmail.com>
2022-12-15 15:00:57 +0100

    optimisation php

--- pages/var.inc.php
+++ pages/var.inc.php
@@ -1,11 +1,11 @@
 <?php
      const title = "vente pc";
      $_img = '<img src="asset/pc.webp" alt="Pc Gamer" loading="lazy">';
-     $_processeur = '<b>Processeur</b> : AMD Ryzen 7 4800H';
-     $_carteG = '<b>Carte Graphique</b> : NVIDIA GeForce RTX 3050Ti';
-     $_ecran = '<b>Ecran</b> : "17,3" (43,9 cm) - Mémoire vive (RAM) : 16 Go';
-     $_ssd = '<b>SSD</b> : SSD 512 Go (en PCi-Express)';
-     $_prix = '<b>Prix</b> : 1299.99€';
+     $_processeur = '<strong>Processeur</strong> : AMD Ryzen 7 4800H';
+     $_carteG = '<strong>Carte Graphique</strong> : NVIDIA GeForce RTX 3050Ti';
+     $_ecran = '<strong>Ecran</strong> : "17,3" (43,9 cm) - Mémoire vive (RAM) : 16 Go';
+     $_ssd = '<strong>SSD</strong> : SSD 512 Go (en PCi-Express)';
+     $_prix = '<strong>Prix</strong> : 1299.99€';
    
    
 ?>
